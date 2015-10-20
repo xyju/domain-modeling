22c7701a7d134c47d1da2ca27067e7c59016747f
xyju <xyju90@gmail.com>
2015-10-20 14:34:25 -0700

    Initial Commit

---
+++ domain-modeling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  domain-modeling
+//
+//  Created by Xiangyu Ju on 15/10/20.
+//  Copyright © 2015年 Xiangyu Ju. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
